9e29bd7da8084c0dd23c53afc177304ccc0a819d
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-06-27 18:27:49 +0200

    sync with mbp (#21)

--- home/dot/lein/profiles.clj
+++ home/dot/lein/profiles.clj
@@ -1,3 +1,8 @@
 {:user {:plugins [[cider/cider-nrepl "0.47.1"]
                   [lein-ancient "1.0.0-RC3"]
-                  [lein-pprint "1.3.2"]]}}
+                  [lein-pprint "1.3.2"]
+                  [com.jakemccrary/lein-test-refresh "0.22.0"]]
+        :test-refresh {:quiet true
+                       :changes-only true}}
+ :repl {:pedantic? :warn}
+ :raw {:plugins ^:replace []}}
