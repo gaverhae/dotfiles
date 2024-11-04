face8408f1b9014f42148bdc67cff6eb9be73749
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-11-04 13:23:06 +0100

    update lein plugins (#47)

--- home/dot/lein/profiles.clj
+++ home/dot/lein/profiles.clj
@@ -1,7 +1,7 @@
-{:user {:plugins [[cider/cider-nrepl "0.47.1"]
+{:user {:plugins [[cider/cider-nrepl "0.50.2"]
                   [lein-ancient "1.0.0-RC3"]
                   [lein-pprint "1.3.2"]
-                  [com.jakemccrary/lein-test-refresh "0.22.0"]]
+                  [com.jakemccrary/lein-test-refresh "0.25.0"]]
         :test-refresh {:quiet true
                        :changes-only true}}
  :repl {:pedantic? :warn}
