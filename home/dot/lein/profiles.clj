3471a81da8fa4243feeae6d79c3c27b18fb74e33
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-04-07 14:22:35 +0200

    lein: add humane-test-output
    Makes reading test failures so much easier.

--- home/dot/lein/profiles.clj
+++ home/dot/lein/profiles.clj
@@ -2,6 +2,9 @@
                   [lein-ancient "1.0.0-RC3"]
                   [lein-pprint "1.3.2"]
                   [com.jakemccrary/lein-test-refresh "0.26.0"]]
+        :dependencies [[pjstadig/humane-test-output "0.11.0"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
         :test-refresh {:quiet true
                        :changes-only true}}
  :repl {:pedantic? :warn}
