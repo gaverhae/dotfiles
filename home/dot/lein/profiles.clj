ba980335f707781cf76f1afe3c7195d382fb6808
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-04-09 15:14:58 +0200

    lein: more discerning options
    `user` is always on; `repl` and `test` are a bit more precise.

--- home/dot/lein/profiles.clj
+++ home/dot/lein/profiles.clj
@@ -1,11 +1,11 @@
-{:user {:plugins [[cider/cider-nrepl "0.54.0"]
-                  [lein-ancient "1.0.0-RC3"]
+{:user {:plugins [[lein-ancient "1.0.0-RC3"]
                   [lein-pprint "1.3.2"]
-                  [com.jakemccrary/lein-test-refresh "0.26.0"]]
-        :dependencies [[pjstadig/humane-test-output "0.11.0"]]
+                  [com.jakemccrary/lein-test-refresh "0.26.0"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.54.0"]]
+        :pedantic? :warn}
+ :test {:dependencies [[pjstadig/humane-test-output "0.11.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
         :test-refresh {:quiet true
                        :changes-only true}}
- :repl {:pedantic? :warn}
  :raw {:plugins ^:replace []}}
