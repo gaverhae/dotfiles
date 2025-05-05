f9b38ffdede418ad61eb92aac9a908f0a8b9a373
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-05-05 11:23:01 +0200

    lein: move cider back to user profile

--- home/dot/lein/profiles.clj
+++ home/dot/lein/profiles.clj
@@ -1,8 +1,8 @@
-{:user {:plugins [[lein-ancient "1.0.0-RC3"]
+{:user {:plugins [[cider/cider-nrepl "0.54.0"]
+                  [lein-ancient "1.0.0-RC3"]
                   [lein-pprint "1.3.2"]
                   [com.jakemccrary/lein-test-refresh "0.26.0"]]}
- :repl {:plugins [[cider/cider-nrepl "0.54.0"]]
-        :pedantic? :warn}
+ :repl {:pedantic? :warn}
  :test {:dependencies [[pjstadig/humane-test-output "0.11.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
