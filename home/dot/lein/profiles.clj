918de3c42ea1459f288d444db234f3c31f76e3b0
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-05-13 11:21:12 +0200

    lein: bump cider (#83)

--- home/dot/lein/profiles.clj
+++ home/dot/lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.54.0"]
+{:user {:plugins [[cider/cider-nrepl "0.55.7"]
                   [lein-ancient "1.0.0-RC3"]
                   [lein-pprint "1.3.2"]
                   [com.jakemccrary/lein-test-refresh "0.26.0"]]}
