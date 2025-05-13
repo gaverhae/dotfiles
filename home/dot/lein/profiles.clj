ed74b56d8e3940a81215e34cbf5c02cd66737d02
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-05-13 11:26:46 +0200

    lein: enable warnings (#85)

--- home/dot/lein/profiles.clj
+++ home/dot/lein/profiles.clj
@@ -2,7 +2,9 @@
                   [lein-ancient "1.0.0-RC3"]
                   [lein-pprint "1.3.2"]
                   [com.jakemccrary/lein-test-refresh "0.26.0"]]}
- :repl {:pedantic? :warn}
+ :repl {:pedantic? :warn
+        :global-vars {*warn-on-reflection* true
+                      *unchecked-math* :warn-on-boxed}}
  :test {:dependencies [[pjstadig/humane-test-output "0.11.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
