7e5494ca917907fb9b1b4e96c1351fa989c56906
Gary Verhaegen <gary.verhaegen@gmail.com>
2026-05-25 09:15:20 +0200

    overwrite existing files

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ pkgs.mkShell {
   LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
   buildInputs = with pkgs; [
     bash
+    coreutils
     curl
     jq
   ];
